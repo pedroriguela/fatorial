86ad06eeb843a3330c75744357c2b081d917c19e
Pedro Leone <origuela.pedro@escalate.com.br>
2022-03-28 14:57:47 -0300

    adicionado modelo recusivo

---
+++ Fatorial Recursivo/Recursivo/Recursivo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Recursivo
+{
+
+	class MainClass
+	{
+
+		public static Int32 fatorial(Int32 num)
+		{
+			if (num == 1)
+			{
+				return 1;
+			}
+			
+			return num * fatorial(num - 1);
+		}
+
+		public static void Main(string[] args)
+		{
+			Int32 num;
+			String entrada;
+
+			Console.Write("Digita um número inteiro: ");
+			entrada = Console.ReadLine();
+			num = Convert.ToInt32(entrada);
+
+			Console.WriteLine("Fatorial de " + num + " é: " + fatorial(num));
+
+		}
+	}
+}
